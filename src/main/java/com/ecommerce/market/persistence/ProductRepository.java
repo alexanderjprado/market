a2336680d083e22b27723cb145f0fb2d99fdf234
LAPTOP-RUVTJMFB\gleni <alexanderprado63@gmail.com>
2022-09-07 15:33:41 -0400

    Query Methods applicated

--- src/main/java/com/ecommerce/market/persistence/ProductRepository.java
+++ src/main/java/com/ecommerce/market/persistence/ProductRepository.java
@@ -4,11 +4,20 @@ import com.ecommerce.market.persistence.crud.ProductCrudRepository;
 import com.ecommerce.market.persistence.entity.Product;
 
 import java.util.List;
+import java.util.Optional;
 
 public class ProductRepository {
     private ProductCrudRepository productCrudRepository;
 
     public List<Product> getAll() {
         return (List<Product>) productCrudRepository.findAll();
+    }
+
+    public List<Product> getByCategoria(int idCategoria) {
+        return productCrudRepository.findByIdCategoriaOrderByNombreAsc(idCategoria);
+    }
+
+    public Optional<List<Product>> getEscasos(int cantidad) {
+        return productCrudRepository.findByCantidadStockLessThanAndEstado(cantidad, true);
     }
 }
--- src/main/java/com/ecommerce/market/persistence/crud/ProductCrudRepository.java
+++ src/main/java/com/ecommerce/market/persistence/crud/ProductCrudRepository.java
@@ -3,6 +3,13 @@ package com.ecommerce.market.persistence.crud;
 import com.ecommerce.market.persistence.entity.Product;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface ProductCrudRepository extends CrudRepository<Product, Integer> {
+    List<Product> findByIdCategoriaOrderByNombreAsc(int idCategoria);
+
+    Optional<List<Product>> findByCantidadStockLessThanAndEstado(int cantidadStock, boolean estado);
+
 
 }
